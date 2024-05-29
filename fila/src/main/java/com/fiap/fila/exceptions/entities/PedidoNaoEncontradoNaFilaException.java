3e4b8bf77004dd05625d29f19f1f8c1bedf8e6fc
mpatusco <m.patusco13@gmail.com>
2024-05-28 23:19:35 -0300

    first commit

--- fila/src/main/java/com/fiap/fila/exceptions/entities/PedidoNaoEncontradoNaFilaException.java
+++
@@ -1,9 +0,0 @@
-package com.fiap.fila.exceptions.entities;
-
-public class PedidoNaoEncontradoNaFilaException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public PedidoNaoEncontradoNaFilaException() {
-        super("Não foi encontrado um pedido com esse número na fila");
-    }
-}
--- fila/src/main/java/com/fiap/fila/interfaces/gateways/IPedidoRepositoryPort.java
+++
@@ -1,9 +0,0 @@
-package com.fiap.fila.interfaces.gateways;
-
-import com.fiap.fila.utils.enums.StatusPedido;
-
-import java.util.UUID;
-
-public interface IPedidoRepositoryPort {
-    void atualizarPedido(UUID idPedido, StatusPedido statusPedido) throws Exception;
-}
